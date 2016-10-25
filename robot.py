3539cb3af9719b96322ba4d66b7e2ed435723297
hrs_gc1 <hrs_gc1@srobo.org>
2016-10-25 11:55:28 +0000

    Add configurable forwards controls Add find_markers

---
+++ robot.py
@@ -0,0 +1,37 @@
+from sr.robot import *
+import time
+
+class MarkerNotFoundError(Exception): pass
+
+class Test(Robot):
+    
+    def __init__(self):
+        print('Start Hobo init')
+        super(Test, self).__init__()
+        print('Robot initialised')
+        self.forwards(1)
+
+    
+    def find_markers(self, minimum=1, max_loop=10):
+        cur = 0
+        markers = []
+        while len(markers) < minimum:
+            cur += 1
+            print("Searching for markers...")
+            markers = self.see()
+            if cur == max_loop:
+                raise MarkerNotFoundError("Marker (minimum {}) not found after {} loops".format(minimum, max_loop))
+        return markers
+        
+    def forwards(self, distance, speed=0.1, ratio=-1, speed_power = 500):        
+        power = speed * speed_power
+        self.motors[0].m0.power = power*ratio
+        self.motors[0].m1.power = power
+        sleep_time = distance / speed
+        time.sleep(sleep_time)
+        self.motors[0].m0.power = 0
+        self.motors[0].m1.power = 0
+
+        
+if __name__ == "__main__":
+    Test()
