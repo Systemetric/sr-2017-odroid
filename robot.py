3934e46b42238f21ff9d549bb15593d84f282764
hrs_gc1 <hrs_gc1@srobo.org>
2016-10-25 12:06:53 +0000

    Decrease speed_power

--- robot.py
+++ robot.py
@@ -23,7 +23,7 @@ class Test(Robot):
                 raise MarkerNotFoundError("Marker (minimum {}) not found after {} loops".format(minimum, max_loop))
         return markers
         
-    def forwards(self, distance, speed=0.1, ratio=-1, speed_power = 500):        
+    def forwards(self, distance, speed=0.1, ratio=-1, speed_power = 50):        
         power = speed * speed_power
         self.motors[0].m0.power = power*ratio
         self.motors[0].m1.power = power
