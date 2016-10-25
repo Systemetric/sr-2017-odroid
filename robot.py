501079e23aea4757e83fc38ff4ae99c96889375f
hrs_gc1 <hrs_gc1@srobo.org>
2016-10-25 12:19:31 +0000

    Change minimum speed

--- robot.py
+++ robot.py
@@ -23,7 +23,7 @@ class Test(Robot):
                 raise MarkerNotFoundError("Marker (minimum {}) not found after {} loops".format(minimum, max_loop))
         return markers
         
-    def forwards(self, distance, speed=0.1, ratio=-1, speed_power = 100):        
+    def forwards(self, distance, speed=0.25, ratio=-1, speed_power = 250):        
         power = speed * speed_power
         self.motors[0].m0.power = power*ratio
         self.motors[0].m1.power = power
