636786f47e3b3b05edfe16199a864d61059e88d9
hrs_sb1 <hrs_sb1@srobo.org>
2016-10-25 13:12:09 +0000

    Fine calibration

--- robot.py
+++ robot.py
@@ -23,7 +23,7 @@ class Test(Robot):
                 raise MarkerNotFoundError("Marker (minimum {}) not found after {} loops".format(minimum, max_loop))
         return markers
         
-    def forwards(self, distance, speed=0.1, ratio=-1, speed_power = 150):        
+    def forwards(self, distance, speed=0.1, ratio=-1.1, speed_power = 160):        
         power = speed * speed_power
         self.motors[0].m0.power = power*ratio
         self.motors[0].m1.power = power
