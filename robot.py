2c6a1f5fa613fe71de0a0c58f589237865f7cff6
hrs_sb1 <hrs_sb1@srobo.org>
2016-10-27 11:42:07 +0000

    Add docstrings

--- robot.py
+++ robot.py
@@ -8,7 +8,11 @@ import math
 class MarkerNotFoundError(Exception): pass
 
 class Test(Robot):#Object
-    
+    """
+    A test framework for the DC motor based robot.
+    When adding functionality such as 'turn' or 'goto_marker', please add it as
+    a function with an appropriate name.
+    """
     def __init__(self):
         print('Start Hobo init')
         super(Test, self).__init__()
@@ -24,6 +28,9 @@ class Test(Robot):#Object
             #print "pol_rot_x", marker.centre.polar.rot_x, "pol_rot_z", marker.centre.polar.rot_y#Marker rotation from robot
             
     def goTo(self, marker):
+        """
+        Go to the given marker
+        """
         lengthOne = float
         print('Markers', marker)
         turnOne = 180 - (marker.orientation.rot_y + marker.centre.polar.rot_y)
@@ -32,7 +39,7 @@ class Test(Robot):#Object
         print('Length from marker', marker.dist)
         lengthTwo = (lengthOne / math.sin(math.radians(90))) * (math.sin(math.radians(marker.orientation.rot_y)))
         print('Length two', lengthTwo)
-        turnTwo = 270
+        turnTwo = 90
         self.turn((turnOne / 2))
         print('Turn one function:',self.turn)
         self.forwards(math.fabs(lengthTwo))
@@ -45,6 +52,12 @@ class Test(Robot):#Object
         
     
     def find_markers(self, minimum=1, max_loop=20):
+        """
+        Find at least `mimimum` markers.
+        If tried `max_loop` times, give up and raise an exception.
+        Things calling `find_markers` should be prepared for this exception and
+        respond to it as necessary.
+        """
         cur = 0
         markers = []
         while len(markers) < minimum:
@@ -55,7 +68,13 @@ class Test(Robot):#Object
                 raise MarkerNotFoundError("Marker (minimum {}) not found after {} loops".format(minimum, max_loop))
         return markers
         
-    def forwards(self, distance, speed=0.75, ratio=-1.05, speed_power = 77):        
+    def forwards(self, distance, speed=0.75, ratio=-1.05, speed_power = 77):
+        """
+        Go forwards `distance` meters at speed `speed` in m/s.
+        ratio and speed_power are for calibration purposes and should not be
+        changed unless the robot stops moving in a straight line at the correct
+        speed
+        """
         power = speed * speed_power
         self.motors[0].m0.power = power*ratio
         self.motors[0].m1.power = power
@@ -67,16 +86,16 @@ class Test(Robot):#Object
         self.motors[0].m1.power = 0
         
     def turn(self, degrees, power=50, ratio=-1, sleep_360=2.14):
+        """
+        Turn `degrees` degrees. (0-360)
+        """
         self.motors[0].m0.power = power*-ratio
         self.motors[0].m1.power = power
         time.sleep(sleep_360/360*degrees)
         self.motors[0].m0.power = 0
         self.motors[0].m1.power = 0
         
-                
-    
 
-        
 if __name__ == "__main__":
     Test()
 
