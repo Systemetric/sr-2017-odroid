224f919f282faa20f5a7a96214c609c247c9b9ba
hrs_sb1 <hrs_sb1@srobo.org>
2016-10-27 11:53:37 +0000

    Add comments

--- robot.py
+++ robot.py
@@ -5,14 +5,27 @@ from sr.robot import *
 import time
 import math
 
-class MarkerNotFoundError(Exception): pass
+class MarkerNotFoundError(Exception):
+    """
+    This exception should always be handled.
+    """
+    pass
 
 class Test(Robot):#Object
-    
+    """
+    A test framework for the DC motor based robot.
+    When adding functionality such as 'turn' or 'goto_marker', please add it as
+    a function with an appropriate name.
+    """
     def __init__(self):
         print('Start Hobo init')
+        # Initialise the motors and powerboard
+        # We're inheriting from sr.robot.Robot so any documentation with robot.*
+        # Should instead be written as self.*
         super(Test, self).__init__()
+        # Completed initialisation
         print('Robot initialised')
+        # Test functionality currently being worked on
         while True:
             marker = self.find_markers(max_loop=20)[0]
             if marker > 0:
@@ -24,8 +37,11 @@ class Test(Robot):#Object
             #print "pol_rot_x", marker.centre.polar.rot_x, "pol_rot_z", marker.centre.polar.rot_y#Marker rotation from robot
             
     def goTo(self, marker):
-        lengthOne = float
-        print('Markers', marker)
+        """
+        Go to the given marker
+        """
+        lengthOne = 1.0
+        print('Marker:', marker)
         turnOne = 180 - (marker.orientation.rot_y + marker.centre.polar.rot_y)
         print('Turn one', turnOne)
         lengthOne = marker.dist
@@ -45,43 +61,60 @@ class Test(Robot):#Object
         
     
     def find_markers(self, minimum=1, max_loop=20):
+        """
+        Find at least `mimimum` markers.
+        If tried `max_loop` times, give up and raise an exception.
+        Things calling `find_markers` should be prepared for this exception and
+        respond to it as necessary.
+        """
         cur = 0
         markers = []
+        # While the amount of markers found is less than the minimum required:
         while len(markers) < minimum:
             cur += 1
-            print("Searching for markers...")
-            markers = self.see()
+            # If maximum tries is hit, give up.
             if cur == max_loop:
                 raise MarkerNotFoundError("Marker (minimum {}) not found after {} loops".format(minimum, max_loop))
+            # Search for markers
+            print("Searching for markers...")
+            markers = self.see()
         return markers
         
-    def forwards(self, distance, speed=0.75, ratio=-1.05, speed_power = 77):        
+    def forwards(self, distance, speed=0.75, ratio=-1.05, speed_power = 77):
+        """
+        Go forwards `distance` meters at speed `speed` in m/s.
+        ratio and speed_power are for calibration purposes and should not be
+        changed unless the robot stops moving in a straight line at the correct
+        speed
+        """
+        # Get the correct power for the speed wanted
         power = speed * speed_power
         self.motors[0].m0.power = power*ratio
         self.motors[0].m1.power = power
+        # Sleep amount of time for motors to have moved correct amount
         sleep_time = distance / speed
-        print "ST",sleep_time, "P", power, "PR", power*ratio
         time.sleep(sleep_time)
-        print "Slept"
+        # Turn off the motors
         self.motors[0].m0.power = 0
         self.motors[0].m1.power = 0
         
     def turn(self, degrees, power=50, ratio=-1, sleep_360=2.14):
+        """
+        Turn `degrees` degrees. (0-360)
+        """
         if degrees >= 180:
             self.motors[0].m0.power = power*-ratio
             self.motors[0].m1.power = power
         else:
             self.motors[0].m0.power = -power*ratio
             self.motors[0].m1.power = -power
-        
+        # Sleep amount of time to turn the correct amount
         time.sleep(sleep_360/360*degrees)
+        # Turn off the motors
         self.motors[0].m0.power = 0
         self.motors[0].m1.power = 0
         
-                
-    
 
-        
 if __name__ == "__main__":
     Test()
 
