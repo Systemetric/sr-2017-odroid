c3dee465f7cfc2c2e6ac0dcc8764a0f859a67850
hrs_sb1 <hrs_sb1@srobo.org>
2016-12-10 11:28:34 +0000

    Wheels last turn

--- robot.py
+++ robot.py
@@ -6,6 +6,7 @@ import time
 import math
 import serial
 import logging
+from mbed_link import StepperMotors
 
 class MarkerNotFoundError(Exception): pass
 
@@ -21,7 +22,8 @@ class Test(Robot):#Object
         self.log.info('Start TobyDragon init')
         super(Test, self).__init__()
         self.log.info('Robot initialised')
-        self.lastTurn = ''
+        self.wheels = StepperMotors(self.log)
+        
         while True:
             self.log.info("Start goto cube.")
             marker = self.find_markers()[0]
@@ -45,14 +47,14 @@ class Test(Robot):#Object
         self.log.debug('marker.centre.polar.rot_y = %s', marker.centre.polar.rot_y) #The angle the marker is from the robot
         self.log.debug('marker.orientation.rot_y = %s', marker.orientation.rot_y) #The rotation of the marker
         turnOne = marker.centre.polar.rot_y #Turns the robot to face the marker
-        self.turn(turnOne)
+        self.wheels.turn(turnOne)
         marker = self.find_markers()[0]
         while math.fabs(marker.centre.polar.rot_y) > 5.0: #If the robot is not facing the marker
             marker = self.find_markers(delta_angle=10)[0]
             self.log.debug('Not correctly aligned')
             self.log.debug('marker.centre.polar.rot_y = %s', marker.centre.polar.rot_y) #The angle the marker is from the robot
             turnOne = marker.centre.polar.rot_y #Turns the robot to face the marker
-            self.turn(turnOne)
+            self.wheels.turn(turnOne)
     
 
     def turnParallelToMarker(self):
@@ -64,7 +66,7 @@ class Test(Robot):#Object
         marker = self.find_markers()[0]
         self.log.debug('marker.centre.polar.rot_y = %s', marker.centre.polar.rot_y)#The angle the marker is from the robot
         self.log.debug('marker.orientation.rot_y = %s', marker.orientation.rot_y)# The rotation of the marker
-        if self.lastTurn == 'Left':#Turns the robot left or right to be perpendicular to the marker
+        if self.wheels.lastTurn == 'Left':#Turns the robot left or right to be perpendicular to the marker
             turnTwo = -(90 -  math.fabs(marker.orientation.rot_y))
             self.log.debug('Turn two, left %s degrees', turnTwo)
         else:
@@ -75,10 +77,10 @@ class Test(Robot):#Object
         self.log.debug('Length from marker to robot: %s', marker.dist)
         lengthTwo = (lengthOne) * math.sin(math.radians(marker.orientation.rot_y))#Works out the length to travel to be perpendicualr to the marker
         self.log.debug('Length two: %s', lengthTwo)
-        self.turn(turnTwo)
+        self.wheels.turn(turnTwo)
         time.sleep(2)
         self.log.info('Moving to be perpendicular to marker')
-        self.forwards(lengthTwo)
+        self.wheels.forwards(lengthTwo)
         
 
     def turnPerpendicularToFaceMarker(self):
@@ -86,21 +88,21 @@ class Test(Robot):#Object
         Given a turn 0f 90 degrees would face towards cube,
         Work out which direction and turn.
         """
-        if  self.lastTurn == 'Left':# Turns left or right depending on which side the marker is
+        if  self.wheels.lastTurn == 'Left':# Turns left or right depending on which side the marker is
             turnThree = 90
             self.log.debug('right turn %s degrees', turnThree)
         else:
             turnThree = -90
             self.log.debug('left turn %s degrees', turnThree)
         self.log.info('Turning to face marker')
-        self.turn(turnThree)
+        self.wheels.turn(turnThree)
         marker = self.find_markers()[0]
         while math.fabs(marker.centre.polar.rot_y) > 5.0: #If the robot is not facing the marker
             marker = self.find_markers(delta_angle=10)[0]
             self.log.debug('Not correctly aligned')
             self.log.debug('marker.centre.polar.rot_y = %s', marker.centre.polar.rot_y)#The angle the marker is from the robot
             turnOne = marker.centre.polar.rot_y #Turns the robot to face the marker
-            self.turn(turnOne)
+            self.wheels.turn(turnOne)
    
 
     def moveToCube(self, minimum_check_distance=0.9):
@@ -120,9 +122,9 @@ class Test(Robot):#Object
             self.log.debug("Marker distance: %s", marker.dist)
             self.log.debug("Marker size: %s", marker.info.size)
             if marker.dist < minimum_check_distance: #If robot is close to the cube, just drive over it
-                self.forwards(distanceFromCube + 1)
+                self.wheels.forwards(distanceFromCube + 1)
                 break
-            self.forwards(distanceFromCube - (distanceFromCube / 3))
+            self.wheels.forwards(distanceFromCube - (distanceFromCube / 3))
             while math.fabs(marker.centre.polar.rot_y) > 5.0: #If the robot is not facing the marker
                 marker = self.find_markers()[0]
                 self.log.debug('Not correctly aligned')
@@ -155,19 +157,19 @@ class Test(Robot):#Object
             
             # If the robot cannot see a marker
             self.log.debug("Searching for markers... (direction = %s)", -delta_angle)
-            self.turn(-delta_angle)
+            self.wheels.turn(-delta_angle)
             markers = self.lookForMarkers(max_loop=max_loop)
             if len(markers) >= minimum:
                 i = 361
                 return markers
             else:                
-                self.turn(delta_angle * 2)
+                self.wheels.turn(delta_angle * 2)
                 i = delta_angle
             while i <= 360:
             # Continue scanning in a circle - probably not in a simple arc
                 self.log.debug("Searching for markers... (direction = %s)", i)
                 markers = self.lookForMarkers(max_loop=max_loop)
-                self.turn(delta_angle)
+                self.wheels.turn(delta_angle)
                 if len(markers) >= minimum:
                     i = 361
                     return markers
@@ -196,89 +198,21 @@ class Test(Robot):#Object
             i += 1
             markers = self.see()
         return markers
-    
-        
-    def forwards(self, distance, speed=0.75, ratio=-1.05, speed_power = 80):
-        """
-        Go forwards (distance) meters
-        """
-        #Sasha Shtyrov decreased the power to the motors to prevent current overload
-        if distance < 0:
-            self.log.warning("robot.forwards() passed a negative distance, inverting!")
-            distance = -distance
-        power = speed * speed_power
-        sleep_time = distance / speed
-        self.log.info("Moving forwards %s meters", distance)
-        #adding lines to start motors at lower power then up to full power
-        self.motors[0].m0.power = power*ratio*0.7
-        self.motors[1].m1.power = power*0.7
-        t_end = time.time() + 0.02
-        while time.time() < t_end:
-            self.log.info("starting %s Amps, starting %s Volts", self.power.battery.current, self.power.battery.voltage)
-            #time.sleep(0.001)
-        #close of code to start slower
-        self.motors[0].m0.power = power*ratio
-        self.motors[1].m1.power = power
-        t_end = time.time() + sleep_time
-        while time.time() < t_end:
-            self.log.info("current draw is %s Amps, voltage draw is %s Volts", self.power.battery.current, self.power.battery.voltage)
-            self.log.info("time left is %s milliseconds", (t_end - time.time()))
-            #time.sleep(0.001) implicit wait from log code
-        self.log.info("current draw is %s Amps, voltage draw is %s Volts", self.power.battery.current, self.power.battery.voltage)
-        #adding lines to slow down motors at lower power then up to zero power
-        self.motors[0].m0.power = power*ratio*0.7
-        self.motors[1].m1.power = power*0.7
-        t_end = time.time() + 0.02
-        while time.time() < t_end:
-            self.log.info("stopping %s Amps, stopping %s Volts", self.power.battery.current, self.power.battery.voltage)
-            time.sleep(0.001)
-        #close of code to stop slower
-        self.motors[0].m0.power = 0
-        self.motors[1].m1.power = 0
-        t_end = time.time() + 0.04
-        while time.time() < t_end:
-            self.log.info("current draw is %s Amps, voltage draw is %s Volts", self.power.battery.current, self.power.battery.voltage)
-
-
-    def turn(self, degrees, power=40, ratio=-1, sleep_360=2.14):
-        """
-        Turn degrees anticlockwise.
-        If passed negative, turn clockwise
-        """
-        if degrees < 0:
-            self.lastTurn = 'Left'
-            power = -power
-            degrees = math.fabs(degrees)
-        else:
-            self.lastTurn = 'Right'
-        if degrees < 25:
-            power = power / 2
-            sleep_360 = sleep_360 * 2
-        self.log.info("Turning %s degrees", degrees)
-        self.motors[0].m0.power = power*-ratio
-        self.motors[1].m1.power = power
-        
-        t_end = time.time() + sleep_360/360*degrees
-        while time.time() < t_end:
-            self.log.info("starting %s Amps, starting %s Volts", self.power.battery.current, self.power.battery.voltage)
-        
-        self.motors[0].m0.power = 0
-        self.motors[1].m1.power = 0
         
     def routeNine(self):      #Code for moving along a route to pick up cubes  B2,C,A1
-        self.forwards(3)      #Move along the edge of arena //Should be 3.5m but for testing its 3m
-        self.turn(90)         #Turn to face 1st marker/cube: B2
+        self.wheels.forwards(3)      #Move along the edge of arena //Should be 3.5m but for testing its 3m
+        self.wheels.turn(90)         #Turn to face 1st marker/cube: B2
         marker = self.find_markers()[0]#Look for the marker/cube: B2 to get distance and rotation
         self.faceMarker(marker)        #Make sure robot is facing cube
         self.moveToCube(marker)        #Drive towards marker/cube:B2
         marker = self.find_markers()[0]#Look for the marker/cube:C which should be in front of the robot
         self.faceMarker(marker)        #Make sure robot is facing marker/cube:C
         self.moveToCube(marker)        #Move towards cube/marker: C
-        self.turn(135)                 #Turn to face cube/marker: A1
+        self.wheels.turn(135)                 #Turn to face cube/marker: A1
         marker = self.find_markers()[0]#Look for the marker/cube: A1 to get distance and rotation
         self.faceMarker(marker)        #Make sure robot is facing marker/cube: A1
         self.moveToCube(marker)        #Move towards cube/marker: A1
-        self.forwards(3.5)             #Move HOME
+        self.wheels.forwards(3.5)             #Move HOME
         
 
     def init_logger(self):
