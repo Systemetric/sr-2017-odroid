eae560151ea9d2d7514b1edac1e970a6e3be140a
hrs_sb1 <hrs_sb1@srobo.org>
2016-12-10 10:53:57 +0000

    Add send_command function to actually send commands to the mbed.

--- mbed_link.py
+++ mbed_link.py
@@ -3,7 +3,8 @@ import serial
 
 
 class StepperMotors():
-    def __init__(self, port, timeout=0.2):
+    def __init__(self, port, logger, timeout=None):
+        self.logger = logger
         self.mbed = serial.Serial(port, timeout=timeout, writeTimeout=timeout)
 
     def move(self, amount):
@@ -32,20 +33,44 @@ class StepperMotors():
         """
         Go forwards `amount` cm.
         """
+        self.send_command("f", amount)
         
     def backwards(self, amount):
         """
         Go backwards `amount` cm.
         """
+        self.send_command("b", amount)
     
     def turn_left(self, amount):
         """
         Turn left `amount` degrees.
         This function is not clever and will turn more than 180 degrees if asked.
         """
+        self.send_command("l", amount)
     
     def turn_right(self, amount):
         """
         Turn right `amount` degrees.
         This function is not clever and will turn more than 180 degrees if asked.
         """
+        self.send_command("r", amount)
+    
+    def send_command(self, command, data):
+        """
+        Send the `command` (character) to the mbed with 1 byte of data (int)
+        """
+        
+        try:
+            self.mbed.write(command)
+            self.mbed.write(chr(data))
+        except serial.SerialTimeoutException:
+            self.log.error("Timeout sending {} character to mbed with data {}. Not retrying.".format(command, data))
+            return "Error"
+        while not self.mbed.inWaiting():
+            response = self.mbed.read(1)
+        if response != "d":
+            self.log.error("Mbed sent us a bad response. May or may not have done `{}`".format(command))
+            return "Error"
+        self.mbed.flushInput()
+        return
+        
