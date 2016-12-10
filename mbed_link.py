660ebd34f01f45240b91f4fbfec72e32a9894aa8
hrs_sb1 <hrs_sb1@srobo.org>
2016-12-10 10:38:34 +0000

    Add more stubs and some documentation

--- mbed_link.py
+++ mbed_link.py
@@ -15,9 +15,37 @@ class StepperMotors():
             self.forwards(amount)
         else:
             self.backwards(amount)
+    
+    def turn(self, amount: int):
+        """
+        Turn amount degrees right (clockwise)
+        This function is clever and will turn the right direction
+        """
+        amount %= 360
+        if amount > 180:
+            self.turn_left(360-amount)
+        else:
+            self.turn_right(amount)
         
+    
     def forwards(self, amount: int):
-        pass
+        """
+        Go forwards `amount` cm.
+        """
         
     def backwards(self, amount: int):
-        pass
+        """
+        Go backwards `amount` cm.
+        """
+    
+    def turn_left(self, amount: int):
+        """
+        Turn left `amount` degrees.
+        This function is not clever and will turn more than 180 degrees if asked.
+        """
+    
+    def turn_right(self, amount: int):
+        """
+        Turn right `amount` degrees.
+        This function is not clever and will turn more than 180 degrees if asked.
+        """
