776302fa73ecb8238719fbaf319a917876fc2a1b
hrs_jh1 <hrs_jh1@srobo.org>
2016-12-21 13:02:45 +0000

    Wait for 5 seconds after mbed response

--- mbed_link.py
+++ mbed_link.py
@@ -1,4 +1,5 @@
 import serial
+import time
 
 
 class StepperMotors():
@@ -81,3 +82,4 @@ class StepperMotors():
             #return "Error"
         self.mbed.flushInput()
         self.log.debug("Sucessfully completed command %s(%s)", command, data)
+        time.sleep(5)
