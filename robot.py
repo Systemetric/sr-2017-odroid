453b2484527e9558b4b8c3ae24f9afd37a50df4b
hrs_jh1 <hrs_jh1@srobo.org>
2016-12-21 14:37:16 +0000

    Switch strategy

--- robot.py
+++ robot.py
@@ -31,7 +31,7 @@ class Test(Robot):
 
     def __init__(self):
         self.init_logger()
-        self.strategy = "test turn once"
+        self.strategy = "test move 4 metres"
         # Please use `log.debug`, `log.info`, `log.warning` or `log.error` instead of `print`
 
         self.log.info('Start TobyDragon init')
