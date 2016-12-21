f04111ec58cbf50e57cdea12439ff8a51ba000fa
hrs_jh1 <hrs_jh1@srobo.org>
2016-12-21 21:09:40 +0000

    Move corrections to a new file

---
+++ corrections.py
@@ -0,0 +1,55 @@
+from math import sin, cos, asin, pi, sqrt, radians
+
+from vector import Vector
+
+
+def correct_for_webcam_horizontal_placement(self, vec):
+    # type: (Vector) -> Vector
+    """
+    Correct for the fact that the webcam is not located at the robot's
+    centre of rotation.
+
+    Takes a vector from the point of view of the webcam, modifies it to be
+    from the point of view of the robot's centre of rotation and returns it.
+
+    See <https://hillsroadrobotics.slack.com/files/anowlcalledjosh/F3GHUJF8D/office_lens_20161219-122405.jpg>
+    or <http://imgur.com/kchEXdP> for a graphical description of how this
+    works.
+    """
+    d = vec.distance
+    alpha = vec.angle
+    r = webcam_offset
+    m = sqrt(d**2 + r**2 - 2 * d * r * cos(pi - alpha))
+    gamma = asin(d * sin(pi - alpha) / m)
+    return Vector(distance=m, angle=gamma)
+
+
+def correct_for_webcam_rotational_placement(self, vec):
+    # type: (Vector) -> Vector
+    """
+    Compensate for the crookedness of the camera.
+    """
+    return Vector(distance=vec.distance, angle=vec.angle - 0.053)
+
+
+def correct_for_cube_marker_placement(self, vec, beta):
+    # type: (Vector, float) -> Vector
+    """
+    Correct for the fact that cube markers are on the edge of the cube, so
+    the centre of the marker isn't the same as the centre of the cube.
+
+    This function requires the angle beta to be passed to it, since it can't
+    be inferred from a vector of d and alpha. It should usually be set to
+    marker.orientation.rot_y.
+
+    See <https://hillsroadrobotics.slack.com/files/anowlcalledjosh/F3GHUJF8D/office_lens_20161219-122405.jpg>
+    or <http://imgur.com/kchEXdP> for a graphical description of how this
+    works.
+    """
+    beta = radians(beta)
+    d = vec.distance
+    alpha = vec.angle
+    r = cube_width / 2
+    m = sqrt(d**2 + r**2 - 2 * d * r * cos(pi - beta))
+    gamma = asin(r * sin(pi - beta) / m)
+    return Vector(distance=m, angle=gamma + alpha)
