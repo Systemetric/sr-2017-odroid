686c7e30e2e13df5a5115268b86cf6b525d97cbc
Josh Holland <anowlcalledjosh@gmail.com>
2016-12-23 21:33:27 +0000

    Improve documentation and remove magic numbers

--- corrections.py
+++ corrections.py
@@ -3,9 +3,13 @@ from math import sin, cos, asin, pi, sqrt, radians
 from vector import Vector
 
 
+# The width/length/height of a cube.
 cube_width = 0.245  # +/- 0.010
-# The distance from the centre of rotation to the webcam.
+# The distance from the centre of rotation to the camera.
 webcam_horizontal_offset = 0.245  # The robot is ~2 cubes long.
+# The angle (in radians) that the camera is angled from the normal by.
+# A positive number means the camera is looking to the right.
+camera_angular_offset = -0.053
 
 
 def correct_all_cube(vec, beta):
@@ -40,8 +44,13 @@ def correct_for_webcam_rotational_placement(vec):
     # type: (Vector) -> Vector
     """
     Compensate for the crookedness of the camera.
+
+    How this works: imagine the camera is directly facing a cube. `vec.angle`
+    is 0, because the robot does not need to turn. Adding a positive number to
+    `vec.angle` means that the robot must actually turn right, presumably
+    because the camera is looking to the right of the centre-line of the robot.
     """
-    return Vector(distance=vec.distance, angle=vec.angle - 0.053)
+    return Vector(distance=vec.distance, angle=vec.angle + camera_angular_offset)
 
 
 def correct_for_cube_marker_placement(vec, beta):
@@ -52,7 +61,7 @@ def correct_for_cube_marker_placement(vec, beta):
 
     This function requires the angle beta to be passed to it, since it can't
     be inferred from a vector of d and alpha. It should usually be set to
-    marker.orientation.rot_y.
+    `radians(marker.orientation.rot_y)`.
 
     See <https://hillsroadrobotics.slack.com/files/anowlcalledjosh/F3GHUJF8D/office_lens_20161219-122405.jpg>
     or <http://imgur.com/kchEXdP> for a graphical description of how this
