0e46221ec36ca54cf46e9d5e90161d86deceef37
hrs_sb1 <hrs_sb1@srobo.org>
2017-01-07 14:17:48 +0000

    Remove `route_nine`

--- strategies.py
+++ strategies.py
@@ -173,30 +173,3 @@ def route_test_vector(robot):
             robot.log.debug("found marker with vector %s", vec)
             robot.log.debug("vector to centre of cube: %s", robot.correct_for_cube_marker_placement(vec, marker.orientation.rot_y))
         time.sleep(5)
-
-
-@strategy(9)
-def route_nine(robot):
-    """
-    Collect cube B, cube C, cube A and return home, in a triangle shape.
-    
-    This is the anticlockwise version of route 9, where the robot first goes
-    along the arena wall to its right, then turns left to collect cubes.
-    """
-    robot.wheels.forwards(3.5)  # Move halfway down the arena
-    robot.wheels.turn(-90)  # Turn left to face into the arena
-    # Get the closest marker of this type. TODO: refactor this into a function.
-    marker = sorted(robot.find_specific_markers(MARKER_TOKEN_B), key=lambda m: m.dist)[0]
-    robot.faceMarker(marker)
-    robot.moveToCube()
-    # Now on top of cube B
-    marker = sorted(robot.find_specific_markers(MARKER_TOKEN_C), key=lambda m: m.dist)[0]
-    robot.faceMarker(marker)  # We *should* be facing there already
-    robot.moveToCube()
-    # Now on top of cube C
-    robot.wheels.turn(-135)  # Now facing cube A/our own corner
-    marker = sorted(robot.find_specific_markers(MARKER_TOKEN_A), key=lambda m: m.dist)[0]
-    robot.faceMarker(marker)
-    robot.moveToCube()
-    # Now on top of cube A, with all cubes collected
-    # TODO: move home, optionally based on wall markers
