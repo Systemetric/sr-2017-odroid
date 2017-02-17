7520f695d4f8fbaeb61a72ec519f52e1d6923ae6
Josh Holland <anowlcalledjosh@gmail.com>
2017-02-17 16:24:13 +0000

    Bypass broken find_marker_approx_position

--- strategies.py
+++ strategies.py
@@ -36,14 +36,16 @@ def route_b_c_a(robot):
     robot.wheels.move(3.25)
     robot.wheels.turn(-90)
     time.sleep(0.2)
-    markers = robot.find_marker_approx_position(MARKER_TOKEN_B, 1.5)
+    # markers = robot.find_marker_approx_position(MARKER_TOKEN_B, 1.5)
     # if markers == []:
     #     # TODO(jdh): do something sane when cubes are missing
     #     robot.log.warn("Can't see B cube! Stopping strategy.")
     #     return
     # else:
-    robot.log.debug("Found %s B cubes, moving to the 0th one", len(markers))
-    marker = markers[0]
+    # robot.log.debug("Found %s B cubes, moving to the 0th one", len(markers))
+    # marker = markers[0]
+    # FIXME
+    marker = robot.find_markers(filter_func=lambda markers: [m for m in markers if m.info.marker_type == MARKER_TOKEN_B and 1.5 - 0.25 <= m.dist <= 1.5 + 0.25])
     robot.log.info("Moving to B cube")
     robot.move_to_cube(marker)
     time.sleep(0.2)
