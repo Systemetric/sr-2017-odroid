c2c02f12033e366979d7c53866034d29d6790c31
hrs_jh1 <hrs_jh1@srobo.org>
2017-03-31 21:07:39 +0000

    Switch strategy

--- robot.py
+++ robot.py
@@ -36,7 +36,7 @@ class CompanionCube(Robot):
         # Please use `log.debug`, `log.info`, `log.warning` or `log.error` instead of `print`
         self.init_logger()
 
-        self.strategy = "test webcam rotational placement correction calibration"
+        self.strategy = "b c a"
         args = []
         kwargs = {"opposite_direction": False}
         self.routeChange = False
